ff8e6755acd3e83cc5b712b5590ec048200fc4ef
chungvle <chungvle@gmail.com>
2015-12-25 11:47:37 -0800

    polymer-compo initial version

---
+++ scripts/app.js
@@ -0,0 +1,51 @@
+/*
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+(function(document) {
+  'use strict';
+
+  // Grab a reference to our auto-binding template
+  // and give it some initial binding values
+  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+  var app = document.querySelector('#city-search-app');
+  console.log('app = ', app);
+
+
+  // Listen for template bound event to know when bindings
+  // have resolved and content has been stamped to the page
+  app.addEventListener('dom-change', function() {
+    console.log('Our app is ready to rock!');
+  });
+
+  // See https://github.com/Polymer/polymer/issues/1381
+  window.addEventListener('WebComponentsReady', function() {
+    // imports are loaded and elements have been registered
+  });
+
+  
+  function supports_history_api() {
+    return !!(window.history && history.pushState);
+    //return (window.history && history.pushState) ? true : false;
+  }   
+  
+  window.onload = function() {
+    if (!supports_history_api()) 
+      return;
+    window.setTimeout(function() {
+      window.addEventListener("popstate", popStateHandler, false);
+    }, 1);
+  };
+  
+  function popStateHandler(e) {
+    console.log('calling stateManager.popStateHandler');
+    var cityinfoComposition = document.querySelector('#cityinfoComposition');
+    cityinfoComposition.popStateHandler(e);
+  }
+
+})(document);
